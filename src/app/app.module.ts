7c53dfdbfaf13ee79b6421068ba7222645b426cf
MicheleSottocasa <michelesottocasa1@gmail.com>
2022-06-15 14:36:14 +0200

    Following guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,24 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetalisComponent } from './product-detalis/product-detalis.component';
-import { CartComponent } from './cart/cart.component';
+import { TopBarComponent } from './component/top-bar/top-bar.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { ProductAlertsComponent } from './component/product-alerts/product-alerts.component';
+import { ProductDetalisComponent } from './component/product-detalis/product-detalis.component';
+import { CartComponent } from './component/cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './component/shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetalisComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent}
     ]),
   ],
   declarations: [
@@ -27,6 +31,7 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductDetalisComponent,
     CartComponent,
+    ShippingComponent,
   ],
   bootstrap: [AppComponent],
 })
