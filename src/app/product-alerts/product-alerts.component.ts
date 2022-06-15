a9429f90eb26c44addc65020527f70dcee09263e
MicheleSottocasa <michelesottocasa1@gmail.com>
2022-06-15 11:32:01 +0200

    5 files modified, 10 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,7 +8,7 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product!: Product;
-  @Output() notify: new EventEmitter();
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
